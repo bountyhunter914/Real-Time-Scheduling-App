bbbda71cb6f93cc8caef22be8f75f4a0788231d7
Jean <jean.vigroux@gmail.com>
2021-09-20 17:45:46 -0400

    Finished framework and routing for the application. All pages are in the page folder and the navigation_bar.dart is just a small class for the navigation bar. Tested on my galaxy s10. Any bugs or problems with the routing please discord dm me.

---
+++ lib/pages/settings_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:real_time_scheduling/navigation_bar.dart';
+
+/// Paul's Page
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      /** Feel free to change the background color.
+       * It is just to help understand which page you are on while implementing your page**/
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        title: const Text('Settings'),
+      ),
+      /** Implement your page in body. Just make sure you leave the NavigationBar**/
+      body: const Align(
+          alignment: Alignment.bottomCenter,
+          child: NavigationBar()),
+
+    );
+  }
+}
