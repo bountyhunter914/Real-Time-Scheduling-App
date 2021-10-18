fe85ea444c94574191eafb7c344e122e4bd65121
chewedupskittle <mlfarrin@buffalo.edu>
2021-10-18 00:36:22 -0400

    Changed the Calendar page due to rollback #17

--- lib/pages/calendar_page.dart
+++ lib/pages/calendar_page.dart
@@ -4,20 +4,24 @@ import 'package:table_calendar/table_calendar.dart';
 
 /// Max's Page
 class CalendarPage extends StatelessWidget {
-  const CalendarPage();
+  const CalendarPage({Key key}) : super(key: key);
+
+  static CalendarController _controller = CalendarController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       /** Feel free to change the background color.
        * It is just to help understand which page you are on while implementing your page**/
-      appBar: AppBar(
-        title: const Text('My Calendar'),
-      ),
-      /** Implement your page in body. Just make sure you leave the NavigationBar**/
-      body: Column(
-
-      ),
-      bottomNavigationBar: NavigationBar(selectedIndex: 1)
+        appBar: AppBar(
+          title: const Text('My Calendar'),
+        ),
+        /** Implement your page in body. Just make sure you leave the NavigationBar**/
+        body: TableCalendar(
+          startDay: DateTime.utc(2010, 10, 16),
+          endDay: DateTime.utc(2030, 3, 14),
+          calendarController: _controller,
+        ),
+        bottomNavigationBar: NavigationBar(selectedIndex: 1)
     );
-  }     
+  }
 }
