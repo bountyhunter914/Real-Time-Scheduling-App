9dac64d480a86f43ff35efbbe08b60ad71b87274
prestonmecca <pdmecca@buffalo.edu>
2021-12-03 14:10:23 -0500

    #37 fixed list viewer for events page

--- lib/pages/test_events_page.dart
+++ lib/pages/test_events_page.dart
@@ -156,24 +156,25 @@ class _EventsMainV2State extends State<EventsMainV2> {
     await helper.event_insert(event);
   }
 
-  Column showEvents(){
-    int cond = 0;
-    if (events.length < 15){
-      cond = events.length;
-    }
-    else{
-      cond = 15;
-    }
+  SingleChildScrollView showEvents(){
     print("ShowEvents Called");
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
+    return SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Expanded(
+                      child: ListView(
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      children: <Widget> [if(this.events != null) for(var i in this.events) ListTile(title: Text(createDate(i[0], i[2], i[3], i[1], i[4], i[5]),
+                        style: TextStyle(color: Colors.white,
+                            backgroundColor: Colors.blueAccent),)) ]
+                  ),
+              )
         //listview.builder would go here ; https://api.flutter.dev/flutter/widgets/ListView-class.html
         //i[0] is event, 1 is year, 2 is month, 3 is day, 4 is hour, minute is 5
-       if(this.events != null) for(var i = 0; i < cond; ++i) Text(createDate(this.events[i][0], this.events[i][2], this.events[i][3], this.events[i][1], this.events[i][4], this.events[i][5]),
-                                                              style: TextStyle(color: Colors.white,
-                                                                              backgroundColor: Colors.blueAccent),)
-      ],
+        ],
+      ),
     );
 
   }
